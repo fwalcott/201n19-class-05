4fe3227606743b9fee70671bf5d701f0bc41d509
Felicia Walcott <fwalcott.walcott@gmail.com>
2020-08-01 16:31:05 -0700

    problem3

--- starter-code/app.js
+++ starter-code/app.js
@@ -48,17 +48,19 @@ Write a function called sumAndMultiply() that takes in three numbers as separate
 Third element: "4 and 7 and 5 sum to 16."
 Fourth element: "The product of 4 and 7 and 5 is 140."
 
-IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to use the + operator for string concatenation.
+IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this. However, you may continue to us e the + operator for string concatenation.
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSumAndMultiply() function and see if the test passes.*/
 
 // Write your code here
-function sumAndMultiply(a, b, c) { //eslint-disable-line
-
+function sumAndMultiply(a, b, c) { //eslint-disable-line 
+var numberArray = [4,7,5] 
+testSum = (`${numberArray[0]} and ${numberArray[1]}and ${numberArray[2]}sum to 16.`); 
+testMultiply = (`${numberArray[0]} and ${numberArray[1]}and ${numberArray[2]}sum to 16.`); 
 }
 
 // Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
